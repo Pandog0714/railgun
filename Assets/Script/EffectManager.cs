23346dcb62a1339ee784d4b71eb54e20371d2181
pan_dog <siba.bike.robot0714@icloud.com>
2022-01-26 17:44:27 +0900

    コミット4・・・EffectManager+アセット追加

---
+++ Assets/Script/EffectManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectManager : MonoBehaviour
+{
+    public static EffectManager instance;
+
+    //Player用
+    [Header("発射口用のエフェクト")]
+    public GameObject muzzleFlashPrefab;
+
+    [Header("敵に弾が当たったときのエフェクト")]
+    public GameObject hitEffectPrefab;
+
+    //TODO 追加
+
+    //Enemy用
+
+    //共通
+
+    private void Awake()
+    {
+        if(instance = null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
