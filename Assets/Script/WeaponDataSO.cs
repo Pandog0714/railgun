6e6bdc7fe73603fc660573adf7dac7aa3073c157
pan_dog <siba.bike.robot0714@icloud.com>
2022-02-25 10:57:57 +0900

    コミット17.2・・・WeaponDateSO追記

--- Assets/Script/WeaponDataSO.cs
+++ Assets/Script/WeaponDataSO.cs
@@ -5,6 +5,19 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "WeaponDataSO", menuName = "Create WeaponDataSO")]
 public class WeaponDataSO : ScriptableObject
 {
+    [System.Serializable]
+    public class WeaponData
+    {
+        public string weaponName;
+        public int weaponNo;
+        public int maxBullet;
+        public float reloadTime;
+        public int bulletPower;
+        public float shootInterval;
+        public float shootRange;
+        public Sprite weaponIcon;
+    }
+
     public List<WeaponData> weaponDatasList = new List<WeaponData>();
 }
 
