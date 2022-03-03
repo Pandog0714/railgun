8e5db3d0c5c85a8f6a5c97a5c88d48ef938ab573
pan_dog <siba.bike.robot0714@icloud.com>
2022-03-03 10:55:16 +0900

    コミット20.3・・・WeaponChanger作成

---
+++ Assets/Script/WeaponChanger.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponChanger : MonoBehaviour
+{
+    public int currentWeaponNo;
+
+    [SerializeField]
+    private PlayerController player;
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {　　//　<=　任意のボタンに設定してください。
+            ChangeWeapon();
+        }
+    }
+
+    /// <summary>
+    /// 武器の交換
+    /// </summary>
+    public void ChangeWeapon()
+    {
+
+        // 初期武器しか所持していない場合
+        if (GameData.instance.weaponDatasList.Count <= 1)
+        {
+
+            Debug.Log("初期武器しかないため、交換しない");
+
+            // 武器交換しない
+            return;
+        }
+
+        currentWeaponNo = ++currentWeaponNo % GameData.instance.weaponDatasList.Count;　　//　<=　☆どういう処理になっているのか、考えてみてください。
+
+        // 武器のデータを更新
+        player.ChangeBulletData(GameData.instance.weaponDatasList[currentWeaponNo]);
+
+        Debug.Log("武器交換");
+    }
+}
