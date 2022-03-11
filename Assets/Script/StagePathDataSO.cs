c7a857f1cb9391cda609d32bc2688d42071ca51c
pan_dog <siba.bike.robot0714@icloud.com>
2022-03-11 14:24:25 +0900

    コミット24・・・StagePathDataSO作成

---
+++ Assets/Script/StagePathDataSO.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// 分岐ごとのパスデータのデータベース
+/// </summary>
+[CreateAssetMenu(fileName = "StagePathDataSO", menuName = "Create StagePathDataSO")]
+public class StagePathDataSO : ScriptableObject
+{
+    [System.Serializable]
+    public class StagePathData
+    {
+
+        [System.Serializable]
+        public class BranchData
+        {
+            public BranchDirectionType branchDirectionType;   //選択肢用の矢印の方向
+            public RailPathData railPathData;
+
+            //TODO他にもあれば追加する
+
+        }
+
+        [Header("分岐ごとのパスデータ群")]
+        public List<BranchData> branchDatasList;
+    }
+
+    [Header("ステージごとのパスデータ群")]
+    public List<StagePathData> stagePathDatasList = new List<StagePathData>();
+}
+
