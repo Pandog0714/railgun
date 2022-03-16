a505097c4099828ea083a9e49c8bf3f41c90532a
pan_dog <siba.bike.robot0714@icloud.com>
2022-03-16 12:32:57 +0900

    コミット24.1・・・DataBaseManager修正

--- Assets/Script/DataBaseManager.cs
+++ Assets/Script/DataBaseManager.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class DataBaseManager : MonoBehaviour
 {
     public static DataBaseManager instance;
 
     public WeaponDataSO weaponDataSO;
+
+    [SerializeField]
+    private StagePathDataSO stagePathDataSO;
 
 
     void Awake()
@@ -23,7 +27,7 @@ public class DataBaseManager : MonoBehaviour
     }
 
     /// <summary>
-    /// WeaponDataSO スクリプタブル・オブジェクトの中から、引数に指定された WeaponNo を持つ WeaponData の取得
+    /// WeaponDataSO,スクリプタブル・オブジェクトの中から、引数に指定されたWeaponNoを持つWeaponDataの取得
     /// </summary>
     /// <param name="searchWeaponNo"></param>
     /// <returns></returns>
@@ -31,4 +35,52 @@ public class DataBaseManager : MonoBehaviour
     {
         return weaponDataSO.weaponDatasList.Find(x => x.weaponNo == searchWeaponNo);
     }
+
+
+    /// <summary>
+    /// ステージパス番号から分岐先のRailPathData情報を取得
+    /// </summary>
+    /// <param name="nextStagePathDataNo"></param>
+    /// <param name="searchBranchDirectionType"></param>
+    /// <returns></returns>
+    public RailPathData GetRailPathDataFromBranchNo(int nextStagePathDataNo, BranchDirectionType searchBranchDirectionType)
+    {
+        return stagePathDataSO.stagePathDatasList[nextStagePathDataNo].branchDatasList.Find(x => x.branchDirectionType == searchBranchDirectionType == searchBranchDirectionType).railPathData;
+    }
+
+    /// <summary>
+    /// ステージ内のルートの数の取得
+    /// </summary>
+    /// <returns></returns>
+    public int GetStagePathDatasListCount()
+    {
+        return stagePathDataSO.stagePathDatasList.Count;
+    }
+
+    /// <summary>
+    /// 分岐の種類の取得
+    /// </summary>
+    /// <param name="nextStagePathDataNo"></param>
+    /// <param name="no"></param>
+    /// <returns></returns>
+    public BranchDirectionType GetBranchDirectionTypeFromRailPathData(int nextStagePathDataNo, int no)
+    {
+        return stagePathDataSO.stagePathDatasList[nextStagePathDataNo].branchDatasList[no].branchDirectionType;
+    }
+
+    /// <summary>
+    /// 全分岐情報の取得
+    /// </summary>
+    /// <param name="nextStagePathDataNo"></param>
+    /// <returns></returns>
+    public BranchDirectionType[] GetBranchDirectionTypes(int nextStagePathDataNo)
+    {
+        return stagePathDataSO.stagePathDatasList[nextStagePathDataNo].branchDatasList.Select(x => x.branchDirectionType).ToArray();
+    }
+
+    public int GetBranchDatasListCount(int branchNo)
+    {
+        return stagePathDataSO.stagePathDatasList[branchNo].branchDatasList.Count;
+    }
+
 }
